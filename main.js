068e7817755211486b5e480b15703eeac3aa75e1
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-14 16:28:18 +0300

    js added function

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-const options = {
-  headers: {
-    "x-access-token":
-      "coinranking72da7f6db2bfdc3c2ea8e95407a1ad27de3540e5c154a74c",
-  },
-};
+const searchInput = document.querySelector("#search");
+const searchBtn = document.querySelector(".btn");
+const cardDiv = document.querySelector(".cardDiv");
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const options = {
+    headers: {
+      "x-access-token":
+        "coinranking72da7f6db2bfdc3c2ea8e95407a1ad27de3540e5c154a74c",
+    },
+  };
 
-fetch("https://api.coinranking.com/v2/blockchains", options)
-  .then((response) => response.json())
-  .then((result) => console.log(result.data.blockchains))
-  .catch((error) => console.log(error));
+  fetch("https://api.coinranking.com/v2/coins", options)
+    .then((response) => response.json())
+    .then((result) => {
+      result.data.coins.forEach((item) => {
+        let cardPrice = parseFloat(item.price).toFixed(2);
+        if (searchInput.value == item.name) {
+          cardDiv.innerHTML += `
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <button><i class="fa-solid fa-x"></i></button>
+                <h3 class="text-light">${item.name}</h3>
+                <p class="text-light">$ <span>${cardPrice}</span></p>
+                <img src="${item.iconUrl}" alt="" />
+                <div class="text-success my-2">
+                    <i class="fa-solid fa-chart-line"></i>
+                    <span>${item.change}%</span>
+                </div>
+            </div>
+          
+          `;
+        }
+      });
+    })
+
+    .catch((error) => console.log(error));
+});
